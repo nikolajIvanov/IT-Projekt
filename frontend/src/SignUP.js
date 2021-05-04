dcb3df5218171bbb952dc36c406311ba4ba9359a
ArditFazliu <af097@hdm-stuttgart.de>
2021-05-04 21:27:52 +0200

    SignUp Komponente von Material-Ui In Classe umgewandelt

--- frontend/src/SignUP.js
+++ frontend/src/SignUP.js
@@ -14,21 +14,21 @@ import PropTypes from "prop-types";
 
 const styles = theme => ({
   paper: {
-    marginTop: theme.spacing(8),
+    marginTop: "theme.spacing(8)",
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
   },
   avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    margin: "theme.spacing(1)",
+    backgroundColor: "theme.palette.secondary.main",
   },
   form: {
     width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
+    marginTop: "theme.spacing(3)",
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    margin: "theme.spacing(3, 0, 2)",
   },
 });
 
