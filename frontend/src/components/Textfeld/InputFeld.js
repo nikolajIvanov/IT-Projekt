01af69d27d34b7d37df244eb45164d75b329e2ce
Chris Schoeller <cs293@hdm-stuttgart.de>
2021-05-13 16:09:20 +0200

    Dropdown und InputFeld Komponenten hinzugefügt und angepasst

---
+++ frontend/src/components/Textfeld/InputFeld.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            width: 200,
+        },
+    },
+}));
+
+export default function InputFeld(props) {
+    const classes = useStyles();
+
+    return (
+        <form className={classes.root} noValidate autoComplete="off">
+            <div>
+                <TextField
+                    id="standard-error-helper-text"
+                    label={props.text}
+                    defaultValue="Hier bitte Eingeben..."
+
+                />
+            </div>
+        </form>
+    );
+}
