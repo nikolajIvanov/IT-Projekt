fab3dc368c0b0f040ba9f55dbdd341e6fde3b3af
nikolajIvanov <62962459+nikolajIvanov@users.noreply.github.com>
2021-05-16 10:50:06 +0200

    Frontend API Klasse erstellt
    Es wurden die einzelnen Methodennamen definiert.

--- frontend/src/api/TeamUpApi.js
+++ frontend/src/api/TeamUpApi.js
@@ -2,4 +2,39 @@
 
 class TeamUpApi {
 
+    static #api = null;
+
+    #serverBaseURL = '/api';
+
+    // User vom Backend holen
+    getUser = (authId) => `${this.#serverBaseURL}/users/${authId}`;
+    // User updaten
+    putUser = (authId) => `${this.#serverBaseURL}/users/${authId}`;
+    // User löschen
+    deleteUser = (authId) => `${this.#serverBaseURL}/users/${authId}`;
+    // Neuen User Anlegen
+    postUser = () => `${this.#serverBaseURL}/users`;
+
+    static getAPI() {
+        if (this.#api == null) {
+        this.#api = new TeamUpApi();
+        }
+        return this.#api;
+    }
+
+
+    #fetchAdvanced = (url, init) => fetch(url, init)
+        .then(res => {
+            // The Promise returned from fetch() won’t reject on HTTP error status even if the response is an HTTP 404 or 500.
+            if (!res.ok) {
+            throw Error(`${res.status} ${res.statusText}`);
+            }
+            return res.json();
+        }
+    )
+
+    getUser(authId) {
+        return
+    }
+
 }
