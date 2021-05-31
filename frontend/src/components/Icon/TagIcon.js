95c81595491d2e5f9f084f1dce86d5f85ae20cdf
Christopher Segatz <49003729+chrissegatz@users.noreply.github.com>
2021-05-31 23:07:33 +0200

    Tag Icon erstellt

---
+++ frontend/src/components/Icon/TagIcon.js
@@ -0,0 +1,21 @@
+import Button from "@material-ui/core/Button";
+import React from 'react';
+
+function tag(props) {
+    return (
+        <div>
+            <Button
+                onClick={props.onClick}
+                style={style.button}>{props.inhalt}</Button>
+        </div>
+    );
+}
+
+export default tag;
+
+const style = {
+    button :{
+        backgroundColor: "grey",
+        color: "black"
+    }
+}
