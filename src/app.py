79f07f4039c2a13e3dba88d2f2b0fd79ba022478
BenitoSchwankhart <39334512+BenitoSchwankhart@users.noreply.github.com>
2021-06-03 11:31:58 +0200

    Bezeichnungen angepasst und fehlende API Schnittstelle deklariert

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ from flask_socketio import SocketIO, send
 from server.api.test_api import TestApi
 from server.api.UsersApi import UsersApi
 from server.api.UserApi import UserApi
+from server.api.LerngruppeApi import LerngruppeApi
 from server.api.NutzerByNameApi import NutzerByNameApi
 from server.api.model import api, app
 
@@ -26,8 +27,8 @@ api.add_resource(UsersApi, '/users')
 api.add_resource(UserApi, '/users/<string:authId>')
 api.add_resource(NutzerByNameApi, '/nutzer-by-name/<string:name>')
 
-api.add_ressource(GruppenApi,'/Lerngruppe')
-api.add_ressource(GruppenApi'/Lerngruppe/<string:')
+api.add_ressource(LerngruppeApi, '/lerngruppe')
+api.add_ressource(LerngruppeApi, '/lerngruppe/<string:name>')
 
 if __name__ == '__main__':
     # app.run(debug=True)
