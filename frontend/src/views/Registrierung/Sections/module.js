ff012220cf81ea34395779d36ad2cf4e96dbeaf1
BenitoSchwankhart <39334512+BenitoSchwankhart@users.noreply.github.com>
2021-06-05 15:47:50 +0200

    Modul angepasst

--- frontend/src/views/Registrierung/Sections/module.js
+++ frontend/src/views/Registrierung/Sections/module.js
@@ -1,51 +1,58 @@
-import React from 'react';
-import {Button, FormControlLabel, FormGroup, Paper, Radio, RadioGroup, Switch, Typography} from "@material-ui/core";
-import Mod from "../../../components/Konstante(DropDown)/Module";
+import React, {useEffect} from 'react';
+import {Button, FormControlLabel, FormGroup, Paper, Switch, Typography} from "@material-ui/core";
 import theme from "../../../theme";
-import DropDown from "../../../components/Textfeld/Dropdown";
+import TeamUpApi from "../../../api/TeamUpApi";
 
-function Module(props) {
+function Module(props){
     const [back, setBack] = React.useState([])
+    const [Mod, setMod] = React.useState([])
 
-    const handleChange = async (mod) => {
+    useEffect( () => {
+        TeamUpApi.getAPI().getModul("Wirtschaftsinformatik und digitale Medien")
+            .then((modul) => {
+                const middle = []
+                modul.forEach(i => {
+                    middle.push({
+                        key: i.getID(),
+                        value: i.getModul()
+                    })
+                })
+                console.log(middle)
+                setMod(middle)
+            })
+    }, []);
+
+    const handleChange = (mod) => {
         if (back.includes(mod) === false) {
             setBack(back => back.concat(mod))
-        } else {
-            //TODO anderes Datenformat
-            setBack(back => [])
+        }
+        else {
+            setBack(back => back.filter(item => item !== mod))
         }
     }
 
     const getResult = () => {
         console.log(back)
-
-        const handleModul = (event) => {
-            props.setModul(event.target.value);
-        };
     }
 
         return (
             <Paper style={props.mode}>
-                <Typography style={theme.font.register}>Was willst du lernen?</Typography>
-                <FormGroup>
-                    {Mod.map((modul, key) =>
+                {Mod ? <>
+                    <Typography style={theme.font.register}>Was willst du lernen?</Typography>
+                    <FormGroup>
+                {Mod.map((modul) =>
                         <FormControlLabel
-                            control={<Switch checked={back.includes(modul)}
-                                             onClick={(e) => {
-                                                 e.preventDefault();
-                                                 handleChange(modul)
-                                             }}/>}
-                            label={modul}
+                            control={<Switch checked={back.includes(modul.value)}
+                            onClick={(e) => {
+                            e.preventDefault();
+                            handleChange(modul.value)
+                            }}/>}
+                        label={modul.value}
                         />
                     )}
-                </FormGroup>
-                <Button onClick={getResult}>Hier</Button>
-                {/*  <DropDown
-                    handleChange = {handleModul}
-                    input = {props.modul}
-                    map = {Mod}
-                    droplabel = {props.drop}
-                />*/}
+                    </FormGroup>
+                    <Button onClick={getResult}>Hier</Button>
+                </> : <Typography>Ups</Typography>}
             </Paper>
         );
 }
