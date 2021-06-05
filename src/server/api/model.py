2022362ab3e3a565f1be324d877883c0f005db87
Ardit Fazliu <70059811+ArditFazliu@users.noreply.github.com>
2021-06-05 15:33:43 +0200

    model.py mitglieder als Int-Wert definiert

--- src/server/api/model.py
+++ src/server/api/model.py
@@ -27,7 +27,7 @@ user = api.inherit('Nutzer', profil, {
 })
 
 lerngruppe = api.inherit('Lerngruppe', profil, {
-    'mitglieder': fields.List(fields.String, attribute=lambda x: x.get_mitglieder(), description='Mitglieder einer Lerngruppe'),
+    'mitglieder': fields.List(fields.Integer, attribute=lambda x: x.get_mitglieder(), description='Mitglieder einer Lerngruppe'),
     'admin': fields.String(attribute=lambda x: x.get_admin(), description='Administrator einer Lerngruppe'),
 })
 
