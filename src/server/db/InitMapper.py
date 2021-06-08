79076e22ba8264a4c2a2a59190be0b3ac8d9d4f5
BenitoSchwankhart <39334512+BenitoSchwankhart@users.noreply.github.com>
2021-06-08 22:27:46 +0200

    Init Backend Kommentiert

--- src/server/db/InitMapper.py
+++ src/server/db/InitMapper.py
@@ -6,6 +6,8 @@ class InitMapper(Mapper):
     def __init__(self):
         super().__init__()
 
+    # Überprüft ob der Nutzer in der Datenbank existiert
+    # --> Es müssen alle Daten vorliegen da Nutzer nur komplett angelegt werden
     def initialize(self, auth_id):
         cursor = self._cnx.cursor(prepared=True)
 
@@ -14,8 +16,8 @@ class InitMapper(Mapper):
         cursor.execute(data_call, (auth_id,))
         user = cursor.fetchone()
 
-        print(user)
-
+        # Wenn der Nutzer nicht vorhanden ist wird der registrierungsprozess im Frontend
+        # initiiert, sonst --> Einfacher Login
         if user is None:
             return 500
         else:
