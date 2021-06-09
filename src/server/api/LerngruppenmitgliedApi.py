4b017ad0c2565d7c0c57b48a3b41f05d326e371d
Ardit Fazliu <70059811+ArditFazliu@users.noreply.github.com>
2021-06-09 14:49:33 +0200

    LerngruppenmitgliedApi.py Methoden angepasst

--- src/server/api/LerngruppenmitgliedApi.py
+++ src/server/api/LerngruppenmitgliedApi.py
@@ -6,13 +6,13 @@ from server.bo.Lerngruppe import Lerngruppe
 
 class LerngruppenmitgliedApi(Resource):
     @api.marshal_with(lerngruppe)
-    def delete(self,):
+    def delete(self):
         adm = Administration()
         proposal = Lerngruppe.from_dict(api.payload)
         return adm.delete_user_by_list(proposal)
 
     @api.marshal_with(lerngruppe)
-    def post(self,):
+    def put(self):
         adm = Administration()
         proposal = Lerngruppe.from_dict(api.payload)
         return adm.create_new_mitglied(proposal)
