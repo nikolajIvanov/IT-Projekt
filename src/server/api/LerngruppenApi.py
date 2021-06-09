8711dca17ed08659c56151a477f9b72f3a52af76
Ardit Fazliu <70059811+ArditFazliu@users.noreply.github.com>
2021-06-09 14:49:05 +0200

    LerngruppenApi.py Errorhandling v0.1

--- src/server/api/LerngruppenApi.py
+++ src/server/api/LerngruppenApi.py
@@ -2,20 +2,29 @@ from .model import lerngruppe, api
 from flask_restx import Resource
 from server.Administration import Administration
 from server.bo.Lerngruppe import Lerngruppe
+from flask import abort
 
 
 class LerngruppenApi(Resource):
     @api.marshal_list_with(lerngruppe)
     def get(self):
         adm = Administration()
-        return adm.get_all_lerngruppen()
+        retrunValue = adm.get_all_lerngruppen()
+        if retrunValue is None:
+            abort(400,'Keine Lerngruppen vorhanden')
+        elif retrunValue[0] is 200:
+            return retrunValue[1]
+        else:
+            abort(retrunValue[0], retrunValue[1])
 
-    @api.marshal_with(lerngruppe)
+
+
+    @api.expect(lerngruppe)
     def post(self):
         adm = Administration()
         proposal = Lerngruppe.from_dict(api.payload)
-
-        if proposal is not None:
-            return adm.create_lerngruppe(proposal)
+        retrunValue = adm.create_lerngruppe(proposal)
+        if retrunValue is 200:
+            return retrunValue
         else:
-            return '', 500
+            abort(retrunValue[0],retrunValue[1])
