5b19780f289b001d346e444866e3e076a6d7b44b
nikolajIvanov <62962459+nikolajIvanov@users.noreply.github.com>
2021-06-12 16:05:54 +0200

    Chatmapper Refactoring V0.2

--- src/server/api/ChatApi.py
+++ src/server/api/ChatApi.py
@@ -7,5 +7,4 @@ class ChatApi(Resource):
 
     @api.marshal_list_with(chat)
     def get(self, room):
-        adm = Administration()
-        return adm.get_chat_by_room(room)
+        return Administration.get_chat_by_room(room)
