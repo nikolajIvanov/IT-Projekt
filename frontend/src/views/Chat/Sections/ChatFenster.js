ee8f159721f9396f3d182116426638ae9bf65ff8
BenitoSchwankhart <39334512+BenitoSchwankhart@users.noreply.github.com>
2021-06-12 17:13:03 +0200

    Chatten funktioniert (fast) Perfekt

--- frontend/src/views/Chat/Sections/ChatFenster.js
+++ frontend/src/views/Chat/Sections/ChatFenster.js
@@ -1,95 +1,160 @@
 import React, {useEffect} from 'react';
 import io from "socket.io-client";
-import H1_bold from "../../../components/Fonts/h1_bold";
 import ButtonPrimary from "../../../components/Button/ButtonPrimary";
 import InputFeld from "../../../components/Textfeld/InputFeld";
-import H3_regular from "../../../components/Fonts/h3_regular";
+import Grid from "@material-ui/core/Grid";
+import {Chip} from "@material-ui/core";
+import ButtonSend from "../../../components/Button/ButtonSend";
+
 /*
-function ChatFenster() {
-    const socket = io.connect()
-    const [message, setMessage] = React.useState("User ist verbunden")
-    const [chat, setChat] = React.useState([])
+function ChatFenster(props) {
+    let socket = null
+    const [data, setData] = React.useState("")
 
-    socket.on('message', msg => receiveMessage(msg))
-    socket.emit('message', {
-        msg: "hi"
-    })
+    useEffect(() => {
+        const endPunkt = "http://localhost:5000/chat"
+        socket = io.connect(endPunkt, {reconnection: true})
+        console.log("Online")
+        socket.on('message', msg => printData(msg))
+        //Socket schließen wenn die Komponente geschlossen wid
+        return socket.close()
+    }, [])
 
-    function receiveMessage(msg){
-        console.log("income" + msg)
+    function printData(msg){
+        console.log(msg)
     }
 
-    function handleMessage(e){
-        setMessage(e.target.value)
+    function sendData(){
+        socket.emit('message', "hi")
     }
 
-    function sendMessage() {
-        console.log("send" + message)
-        socket.emit('message', {
-            msg: message
-        })
+    function handleData(event){
+        setData(event.target.value)
     }
 
     return (
         <div>
             <H1_bold inhalt={"HI"}/>
-            <H3_regular inhalt={chat[0]}/>
-            <InputFeld onChange={handleMessage} value={message}/>
-            <ButtonPrimary onClick={sendMessage} inhalt={"Senden"}/>
+            <InputFeld onChange={handleData} value={data}/>
+            <ButtonPrimary onClick={sendData} inhalt={"Senden"}/>
         </div>
     );
 }
 
 export default ChatFenster;
 */
-class Dashboard extends React.Component {
-    state = {
-        socketData: "",
-        socketStatus:"On"
+
+
+class ChatFenster extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            myId: 1,
+            partnerId: 2,
+            sendData: "",
+            //Nur zur Demo
+            sendData2: "",
+            myData: [],
+            partnerData: [],
+            chat:[]
+        }
     }
+
+    //TODO statt Grid --> Liste machen
+
     componentWillUnmount() {
         this.socket.close()
-        console.log("component unmounted")
     }
+
     componentDidMount() {
-        var sensorEndpoint = "http://localhost:5000/chat"
+        //API call für alle Chats die schon existieren mit
+        // if else für eigene und Partner Daten speichern in variablen
+        const sensorEndpoint = "http://localhost:5000/chat"
             this.socket = io.connect(sensorEndpoint, {
             reconnection: true,
-            // transports: ['websocket']
         });
-        console.log("component mounted")
-            this.socket.on("responseMessage", message => {
-                this.setState({'socketData': message.temperature})
-
-                console.log("responseMessage", message)
+        console.log("Chat ist offen")
+            this.socket.on("message", message => {
+                this.handlePartner(message)
+                console.log("message", message)
             })
+    }
 
+    handlePartner = (message) => {
+        //hier soll die eigene Id eingesetzt werden
+        if(message.userId === 1){
+            this.setState({
+                chat: [...this.state.chat, (
+                    <Grid item className="leftChat" sx={6}>
+                        <Chip color="primary" className="Chatbox" label={message.message} />
+                    </Grid>
+                )]
+            })
+        }
+        if(message.userId === 2){
+            this.setState({
+                chat: [...this.state.chat, (
+                    <Grid item className="rightChat" sx={6}>
+                        <Chip color="secondary" className="Chatbox" label={message.message} />
+                    </Grid>
+                )]
+            })
+        }
     }
 
     handleMessage = (e) =>{
         this.setState({
-            socketData: e.target.value
+            sendData: e.target.value
         })
     }
-    handleEmit=()=>{
-        if(this.state.socketStatus==="On"){
-        this.socket.emit("message", {'roomId': 1, 'message': this.state.socketData, 'userId': 1})
-        this.setState({'socketStatus':"Off"})
+
+    //Nur zu DemoZwecken
+    handleMessage2 = (e) =>{
+        this.setState({
+            sendData2: e.target.value
+        })
     }
-    else{
-        this.socket.emit("message", {'roomId': 1, 'message': this.state.socketData, 'userId': 1})
-        this.setState({'socketStatus':"On"})
-        }
+
+    handleSend1 = () => {
+        this.socket.emit("message", {
+            roomId: 1,
+            message: this.state.sendData,
+            userId: this.state.myId})
         console.log("Emit Clicked")
+        this.setState({sendData: ""})
     }
+
+    //Nur zu Demo-Zwecken
+    handleSend2 = () => {
+        this.socket.emit("message", {
+            roomId: 1,
+            message: this.state.sendData2,
+            userId: this.state.partnerId})
+        console.log("Emit Clicked")
+        this.setState({sendData2: ""})
+    }
+
     render() {
+        const {chat, sendData, sendData2} = this.state
         return (
-            <React.Fragment>
-                <H1_bold inhalt={"HI"}/>
-            <InputFeld onChange={this. handleMessage} value={this.state.socketData}/>
-            <ButtonPrimary onClick={this.handleEmit} inhalt={"Senden"}/>
-            </React.Fragment>
+            <div className="card">
+                <Grid container className="chatOutlines">
+                        {chat.map((chat) =>
+                            <div>{chat}</div>
+                        )}
+                    <Grid item sx={12} className="sendBox">
+                        <ButtonSend onClick={this.handleSend1} onChange={this.handleMessage}
+                                    inhalt={sendData}/>
+                    </Grid>
+                </Grid>
+                <Grid container className="chatOutlines">
+                    <Grid item sx={12}>
+                        <InputFeld onChange={this.handleMessage2} inhalt={sendData2}/>
+                        <ButtonPrimary onClick={this.handleSend2} inhalt={"Senden"}/>
+                    </Grid>
+                </Grid>
+            </div>
         )
     }
 }
-export default Dashboard;
+export default ChatFenster;
