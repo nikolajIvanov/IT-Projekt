3998542b5c9769df47b2497b777fab95ebc5b5d3
Ardit Fazliu <70059811+ArditFazliu@users.noreply.github.com>
2021-06-24 20:02:22 +0200

    PEP8 Refactoring

--- src/SecurityDecorator.py
+++ src/SecurityDecorator.py
@@ -1,8 +1,7 @@
-import os
+
 from flask import request
 from google.auth.transport import requests
 import google.oauth2.id_token
-from server.Administration import Administration
 
 
 def secured(function):
